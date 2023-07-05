0af64c111ba4aed2415705b81a4f145491f71ebc
JaguarJack <njphper@gmail.com>
2023-07-05 17:15:07 +0800

    feat:add expose header

--- src/Middleware/JsonResponseMiddleware.php
+++ src/Middleware/JsonResponseMiddleware.php
@@ -5,6 +5,7 @@ namespace Catch\Middleware;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class JsonResponseMiddleware
 {
@@ -12,6 +13,15 @@ class JsonResponseMiddleware
     {
         $response = $next($request);
 
+        // set expose header，download excel needs
+        $response->headers->set('Access-Control-Expose-Headers', 'filename,write_type');
+
+        // binary file response
+        if ($response instanceof BinaryFileResponse) {
+            return $response;
+        }
+
+        // other response
         if ($response instanceof Response) {
             return new JsonResponse($response->getContent());
         }
