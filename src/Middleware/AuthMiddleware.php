d5a64e1172cf1cf359923e06f4db4baec75291df
JoJo <njphper@gmail.com>
2026-01-06 08:05:02 +0800

    fix: 修复身份验证校验

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -5,27 +5,29 @@ namespace Catch\Middleware;
 use Catch\Enums\Code;
 use Catch\Events\User as UserEvent;
 use Catch\Exceptions\FailedException;
-use Exception;
+use Catch\Exceptions\LostLoginException;
+use Catch\Exceptions\TokenExpiredException;
+use Catch\Facade\Admin;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
-use Throwable;
 
 class AuthMiddleware
 {
     public function handle(Request $request, \Closure $next)
     {
         try {
-            if (! $user = Auth::guard(getGuardName())->user()) {
-                throw new AuthenticationException();
-            }
-
-            Event::dispatch(new UserEvent($user));
-
-            return $next($request);
-        } catch (Exception|Throwable $e) {
-            throw new FailedException(Code::LOST_LOGIN->message().":{$e->getMessage()}", Code::LOST_LOGIN);
+            $user = Admin::auth();
+        } catch (AuthenticationException $e) {
+            throw new LostLoginException('身份认证过期或失败');
+        } catch (TokenExpiredException $e) {
+            throw new LostLoginException('Token 已过期');
         }
+
+        if ($user) {
+            Event::dispatch(new UserEvent($user));
+        }
+
+        return $next($request);
     }
 }
