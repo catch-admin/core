966489334c13c0cec97d561f3764b675e2483fe4
JoJo <njphper@gmail.com>
2026-01-13 18:35:39 +0800

    feat: 优化模块基类服务

--- src/Providers/CatchModuleServiceProvider.php
+++ src/Providers/CatchModuleServiceProvider.php
@@ -3,7 +3,7 @@
 // +----------------------------------------------------------------------
 // | CatchAdmin [Just Like ～ ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2017 ~ now https://catchadmin.com All rights reserved.
+// | Copyright (c) 2017 ~ now https://catchadmin.vip All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( https://github.com/JaguarJack/catchadmin/blob/master/LICENSE.md )
 // +----------------------------------------------------------------------
@@ -23,38 +23,48 @@ abstract class CatchModuleServiceProvider extends ServiceProvider
 {
     protected array $events = [];
 
+    protected array $commands = [];
 
     /**
      * register
+     *
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
-     * @return void
      */
     public function register(): void
     {
-        foreach ($this->events as $event => $listener) {
-            Event::listen($event, $listener);
-        }
-
+        $this->registerEvents();
         $this->loadMiddlewares();
-
         $this->loadModuleRoute();
-
         $this->loadConfig();
+        $this->commands($this->commands);
+    }
+
+    protected function registerEvents(): void
+    {
+        foreach ($this->events as $event => $listener) {
+            if (is_array($listener)) {
+                foreach ($listener as $val) {
+                    Event::listen($event, $val);
+                }
+            } else {
+                Event::listen($event, $listener);
+            }
+        }
     }
 
     /**
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    protected function loadMiddlewares()
+    protected function loadMiddlewares(): void
     {
         if (! empty($middlewares = $this->middlewares())) {
             $route = $this->app['config']->get('catch.route', [
-                'middlewares' => []
+                'middlewares' => [],
             ]);
 
-            $route['middlewares']= array_merge($route['middlewares'], $middlewares);
+            $route['middlewares'] = array_merge($route['middlewares'], $middlewares);
 
             $this->app['config']->set('catch.route', $route);
         }
@@ -63,7 +73,7 @@ abstract class CatchModuleServiceProvider extends ServiceProvider
     /**
      * load module config
      */
-    protected function loadConfig()
+    protected function loadConfig(): void
     {
         if (! is_dir($configPath = $this->configPath())) {
             return;
@@ -74,16 +84,12 @@ abstract class CatchModuleServiceProvider extends ServiceProvider
             $files[str_replace('.php', '', $file->getBasename())] = $file->getRealPath();
         }
 
-        // multi config files
+        // 加载模块配置
         foreach ($files as $name => $file) {
-            $this->app->make('config')->set(sprintf('%s.%s',$this->moduleName(), $name), require $file);
+            $this->app->make('config')->set(sprintf('%s.%s', lcfirst($this->moduleName()), $name), require $file);
         }
     }
 
-    /**
-     *
-     * @return array
-     */
     protected function middlewares(): array
     {
         return [];
@@ -91,6 +97,7 @@ abstract class CatchModuleServiceProvider extends ServiceProvider
 
     /**
      * return void
+     *
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
@@ -105,19 +112,14 @@ abstract class CatchModuleServiceProvider extends ServiceProvider
 
     /**
      * route path
-     *
-     * @return string|array
      */
-    abstract protected function moduleName(): string | array;
-
+    abstract protected function moduleName(): string|array;
 
     /**
      * module config path
-     *
-     * @return string
      */
     protected function configPath(): string
     {
-        return CatchAdmin::getModulePath($this->moduleName()) . 'config' . DIRECTORY_SEPARATOR;
+        return CatchAdmin::getModulePath($this->moduleName()).'config'.DIRECTORY_SEPARATOR;
     }
 }
