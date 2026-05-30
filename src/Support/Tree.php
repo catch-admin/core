085c4a2d47cf200ee7d1eb6c65aed86c9c7df54f
jaguarjack <njphper@gmail.com>
2026-05-30 08:25:45 +0800

    优化树结构性能

--- src/Support/Tree.php
+++ src/Support/Tree.php
@@ -3,13 +3,12 @@
 // +----------------------------------------------------------------------
 // | CatchAdmin [Just Like ～ ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2017~2021 https://catchadmin.com All rights reserved.
+// | Copyright (c) 2017~2021 https://catchadmin.vip All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( https://github.com/JaguarJack/catchadmin-laravel/blob/master/LICENSE.md )
 // +----------------------------------------------------------------------
 // | Author: JaguarJack [ njphper@gmail.com ]
 // +----------------------------------------------------------------------
-
 
 declare(strict_types=1);
 
@@ -19,41 +18,44 @@ class Tree
 {
     protected static string $pk = 'id';
 
-    /**
-     *
-     * @param string $pk
-     * @return Tree
-     */
     public static function setPk(string $pk): Tree
     {
         self::$pk = $pk;
 
-        return new self();
+        return new self;
     }
 
     /**
      * return done
-     *
-     * @param array $items
-     * @param int $pid
-     * @param string $pidField
-     * @param string $child
-     * @return array
      */
-    public static function done(array $items, int $pid = 0, string $pidField = 'parent_id', string $child = 'children'): array
+    public static function done(array $items, int $pid = 0, string $pidField = 'parent_id', string $child = 'children', $id = 'id'): array
+    {
+        self::$pk = $id;
+
+        $childrenByPid = [];
+
+        foreach ($items as $item) {
+            $childrenByPid[(int) $item[$pidField]][] = $item;
+        }
+
+        return self::build($childrenByPid, $pid, $child);
+    }
+
+    /**
+     * build tree
+     */
+    private static function build(array &$childrenByPid, int $pid, string $child): array
     {
         $tree = [];
 
-        foreach ($items as $item) {
-            if ($item[$pidField] == $pid) {
-                $children = self::done($items, $item[self::$pk], $pidField, $child);
+        foreach ($childrenByPid[$pid] ?? [] as $item) {
+            $children = self::build($childrenByPid, (int) $item[self::$pk], $child);
 
-                if (count($children)) {
-                    $item[$child] = $children;
-                }
-
-                $tree[] = $item;
+            if (count($children)) {
+                $item[$child] = $children;
             }
+
+            $tree[] = $item;
         }
 
         return $tree;
